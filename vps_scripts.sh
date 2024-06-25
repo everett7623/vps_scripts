6adcfbf9785fdbce356f595385448c9479f14d50
Jensfrank <everett7623@gmail.com>
2024-06-25 13:36:59 +0800

    Update vps_scripts.sh

--- vps_scripts.sh
+++ vps_scripts.sh
@@ -78,7 +78,7 @@ total_count=$(cat "$COUNT_FILE")
 
 clear
 # 输出欢迎信息
-echo -e "今日运行次数: ${RED}$daily_count${NC}，累计运行次数: ${RED}$total_count${NC}"
+echo -e "今日运行次数: ${RED}$daily_count${NC} 次，累计运行次数: ${RED}$total_count${NC} 次"
 echo ""
 echo -e "${YELLOW}---------------------------------By'Jensfrank---------------------------------${NC}"
 echo ""
