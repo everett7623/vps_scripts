0f257f88554cf0a56429a61eea1a06e141f7269c
Jensfrank <everett7623@gmail.com>
2024-07-07 01:34:17 +0800

    Update vps_scripts.sh

--- vps_scripts.sh
+++ vps_scripts.sh
@@ -167,7 +167,7 @@ add_alias() {
 
 # 统计使用次数
 sum_run_times() {
-    local COUNT=$(wget --no-check-certificate -qO- --tries=2 --timeout=2 "https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Feverett7623%2Fvps_scripts%2Fmain%2Fvps_scripts.sh" 2>&1 | grep -m1 -oE "[0-9]+[ ]+/[ ]+[0-9]+")
+    local COUNT=$(wget --no-check-certificate -qO- --tries=2 --timeout=2 "https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Feverett7623%2Fvps_scripts%2Fblob%2Fmain%2Fvps_scripts.sh" 2>&1 | grep -m1 -oE "[0-9]+[ ]+/[ ]+[0-9]+")
     if [[ -n "$COUNT" ]]; then
         daily_count=$(cut -d " " -f1 <<< "$COUNT")
         total_count=$(cut -d " " -f3 <<< "$COUNT")
