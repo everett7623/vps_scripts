46e91872969e5a183a01318e1d4facadb6e11715
Jensfrank <everett7623@gmail.com>
2025-06-23 17:19:05 +0800

    Update aapanel.sh

--- scripts/service_install/aapanel.sh
+++ scripts/service_install/aapanel.sh
@@ -4,7 +4,7 @@
 # 脚本描述: aaPanel（宝塔国际版）官方安装脚本增强版
 # 脚本路径: vps_scripts/scripts/service_install/aapanel.sh
 # 作者: Jensfrank
-# 使用方法: bash aapanel_official.sh [选项]
+# 使用方法: bash aapanel.sh [选项]
 # 选项说明:
 #   --user <用户名>      自定义用户名
 #   --password <密码>    自定义密码
